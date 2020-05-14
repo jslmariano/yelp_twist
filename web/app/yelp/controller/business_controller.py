efdcfa6a64f4bcff41d07038376606a08a85ed89
Josel Mariano <jslmariano@gmail.com>
2020-05-14 11:20:54 +0800

    Working google vision api with service acount key file

--- web/app/yelp/controller/business_controller.py
+++ web/app/yelp/controller/business_controller.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restplus import Resource
 
 from ..util.dto import BusinessDto
+from ..service.google_vision_service import test_google_vision
 
 api = BusinessDto.api
 _business = BusinessDto.business
@@ -21,5 +22,6 @@ class BusinessCsv(Resource):
     @api.doc('just_a_test')
     def get(self):
         """List all registered users"""
+        test_google_vision()
         return {'message': 'Hello, I am your backend'}
 
