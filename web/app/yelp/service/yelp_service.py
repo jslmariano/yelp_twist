048097555999be8e0732e6332e9de0df4c519d24
Josel Mariano <jslmariano@gmail.com>
2020-05-14 13:25:14 +0800

    * Added nginx static html files * Added nginx routing for static files * Added yelp credential loader from json file

--- web/app/yelp/service/yelp_service.py
+++ web/app/yelp/service/yelp_service.py
@@ -1,24 +1,43 @@
 import io
 import os
+import json
+from urllib.parse import urlencode
 
 import requests
 
 
 YELP_DOMAIN = "https://api.yelp.com"
-YELP_API_BUSINESS_SEARCH = "businesses/search"
+YELP_API_BUSINESS_SEARCH = "v3/businesses/search"
 
 def test_yelp():
 
-    url = "https://api.yelp.com/v3/businesses/search?term=spiral&latitude=14.6091&longitude=121.0223"
+    filters = dict()
+    # MANILA !!
+    filters['latitude'] = '14.6091'
+    filters['longitude'] = '121.0223'
+
+    filters['term'] = 'spiral'
+
+    print(get_formated_api(filters))
+    print(os.getenv('YELP_CREDENTIALS', None))
+    print(get_yelp_credential(os.path.abspath(os.getenv('YELP_CREDENTIALS', None))))
+
+    yelp_credential = get_yelp_credential(os.path.abspath(os.getenv('YELP_CREDENTIALS', None)))
 
     payload = {}
     headers = {
-      'Authorization': 'Bearer dochXjYD-AT9mGVgd83tk9opUlJQ3ARfEPxX47puAfSSYn6KAQUFlsDRFGVeVgkJAXs_KKEwCCPv4xlZWfvM33REymKhBlScdrN2k9clCaTWssbEd3nQiYCzcT28XnYx',
+      'Authorization': "Bearer {}".format(yelp_credential['api_key']),
     }
 
-    response = requests.request("GET", url, headers=headers, data = payload)
+    response = requests.request("GET", get_formated_api(filters), headers=headers, data = payload)
 
     return response.json()
 
-def get_formated_api():
-    return "{}/{}".format(YELP_DOMAIN, YELP_API_BUSINESS_SEARCH)
+def get_formated_api(query_params):
+    query_params = urlencode(query_params)
+    return "{}/{}?{}".format(YELP_DOMAIN, YELP_API_BUSINESS_SEARCH, query_params)
+
+def get_yelp_credential(filename):
+    with open(filename) as f_in:
+       return(json.load(f_in))
+
