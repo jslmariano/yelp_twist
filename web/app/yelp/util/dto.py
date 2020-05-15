fc8d4e3ba5cad0aa1e8db697017be6821a8c4367
Josel Mariano <jslmariano@gmail.com>
2020-05-15 10:23:16 +0800

    Added CICD workflow

--- web/app/yelp/util/dto.py
+++ web/app/yelp/util/dto.py
@@ -1,6 +1,7 @@
+# Flask
 from flask_restplus import Namespace, fields
 
-
+# Namespace
 class BusinessDto:
     api = Namespace('business', description='business related operations')
     business = api.model('business', {
